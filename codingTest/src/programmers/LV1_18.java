d1bae96e9f96a43738b286fbab7d83ce48e16034
YunYoungCho <yoonyoung37@naver.com>
2023-05-27 00:07:35 +0900

    20230526_Lv1_18_달리기 경주

--- codingTest/src/programmers/LV1_18.java
+++ codingTest/src/programmers/LV1_18.java
@@ -7,7 +7,6 @@ public class LV1_18 {
     // 달리기 경주
     // 20230526
     public String[] solution(String[] players, String[] callings) {
-        //String[] answer = new String[players.length];
 
         // 1. hashmap에 넣어주기
         HashMap<String, Integer> score = new HashMap<>();
@@ -19,8 +18,8 @@ public class LV1_18 {
         for (int i = 0; i < callings.length; i++) {
             String name = callings[i]; // 추월한 사람
             int chuwal = score.get(name); // 추월한 사람의 원래 순서
-            int dchuwal = chuwal - 1;
-            String name1 = getKey(score, dchuwal); // 추월당한 사람
+            int dchuwal = chuwal - 1; // 추월당한 사람의 원래 순서
+            String name1 = players[dchuwal]; // 추월당한 사람(player에서 찾아도 됨)
 
             score.replace(name, dchuwal);
             score.replace(name1, chuwal);
@@ -28,23 +27,6 @@ public class LV1_18 {
             players[dchuwal]=name;
             players[chuwal]=name1;
         }
-
-        // 3. answer에 넣어주기
-        // for (int i = 0; i < players.length; i++) {
-        //     String name = getKey(score, i);
-        //     answer[i] = name;
-        // }
         return players;
-    }
-
-    // hashmap에 value 로 key 찾기
-    public <K, V> K getKey(Map<K, V> map, V value) {
-
-        for (K key : map.keySet()) {
-            if (value.equals(map.get(key))) {
-                return key;
-            }
-        }
-        return null;
     }
 }

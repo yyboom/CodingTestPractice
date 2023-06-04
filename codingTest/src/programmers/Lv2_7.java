836cf2e5c19b751fe7675319aefb2f0b466fc0b6
YunYoungCho <yoonyoung37@naver.com>
2023-06-05 00:08:24 +0900

    20230604_Lv2_7_카펫

--- codingTest/src/programmers/Lv2_7.java
+++ codingTest/src/programmers/Lv2_7.java
@@ -1,27 +1,29 @@
 package programmers;
+
 public class Lv2_7 {
     // 카펫
     // 20230604
     public int[] solution(int brown, int yellow) {
-        int[] answer =new int[2];
+        int[] answer = new int[2];
 
         // 1. 면적의 넓이가 큰 순서대로 탐색함.
         int s = brown + yellow; // 전체 면적
 
-        for(int i=3;i<s;i++){ // 격자를 가지기 위해선 가로 세로가 길이가 3이상이 되어야 함.
-            int j = s/i;
+        for (int i = 3; i < s; i++) { // 격자를 가지기 위해선 가로 세로가 길이가 3이상이 되어야 함.
+            int j = s / i;
 
-            if(s%i==0&&j>=3){
-            int yy = (i-2) * (h-2); // 노란 카펫의 넓이
-            int bb = s-yy; // 갈색 카펫의 넓이
+            if (s % i == 0 && j >= 3) {
+                int sero = Math.min(j, i);
+                int garo = Math.max(i, j); // 가로가 세로보다 길거나 같음
 
-            // 찾았을 때
-            if(yellow == yy && brown == bb){
-                answer[0] = i;
-                answer[1] = h;
-                break;
+                int yy = (sero - 2) * (garo - 2);
+
+                // 찾았을 때
+                if (yellow == yy) {
+                    answer[0] = garo;
+                    answer[1] = sero;
+                }
             }
-        }
         }
         return answer;
     }

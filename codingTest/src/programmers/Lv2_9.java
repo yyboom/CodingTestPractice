f760b311a357ed2d0361c51473363a9a41fefd52
YunYoungCho <yoonyoung37@naver.com>
2023-06-07 12:58:41 +0900

    20230607_Lv2_9_전력망을 둘로 나누기

--- codingTest/src/programmers/Lv2_9.java
+++ codingTest/src/programmers/Lv2_9.java
@@ -6,54 +6,64 @@ public class Lv2_9 {
     // 전력망을 둘로 나누기
     // 20230606
     static ArrayList<Integer>[] graph; // arraylist에 배열 넣어주는 방법
+    static boolean[] visited;
+    static int answer;
     public static int solution(int n, int[][] wires) {
-        int answer = 10000;
+        answer = 10000;
 
         // 1. 방향이 없는 그래프에 넣어주기
         // 그래프를 형성하는 방법: 인접행렬, 인접리스트(배열의 크기가 크면 사용)
         graph = new ArrayList[n+1]; // arraylist에 배열 넣어주는 방법
+        visited = new boolean[n+1]; // 0은 언제나 false라는 것을 기억
         for(int i=1;i<n+1;i++){
             graph[i] = new ArrayList<Integer>();
         }
 
         for(int i=0;i<wires.length;i++){
             graph[wires[i][0]].add(wires[i][1]);
+            graph[wires[i][1]].add(wires[i][0]);
         }
 
         // 2. 연결된 것을 끊어가면서 차이를 계산
         for(int i=0;i<wires.length;i++){
             graph[wires[i][0]].remove(Integer.valueOf(wires[i][1])); // index가 아닌 특정 value값을 삭제
-            boolean[] visited = new boolean[n+1]; // 0은 언제나 false라는 것을 기억
+            graph[wires[i][1]].remove(Integer.valueOf(wires[i][0]));
+            visited[wires[i][0]] = true;
+            int cnt1=1;
 
             // 탐색
-            for(int j=1;j<graph.length;j++){
-                for(int h : graph[j]){
-                    if(!visited[h]) visited[h] = true;
-                }
-            }
-
-            // true인 노드끼리 연결, 0을 제외한 false인 노드끼리 연결되어있음.
-            int result = 0;
-            for(int j=1;j<visited.length;j++){
-                if(visited[j]) result++;
-            }
+            int cnt = bfs(wires[i][0],cnt1);
 
             // 3. min값을 계속해서 갱신해줌
-            System.out.println("result"+result);
-            System.out.println("an"+(n-result));
-            int abs = Math.abs(result-(n-result));
+            int abs = Math.abs(cnt-(n-cnt));
             answer = Math.min(answer, abs);
 
             // 다시 원래대로 연결
             graph[wires[i][0]].add(wires[i][1]);
+            graph[wires[i][1]].add(wires[i][0]);
+            for(int j=0;j<n+1;j++) visited[j] = false;
+            cnt=0;
         }
 
         return answer;
     }
 
+    public static int bfs(int h,int cnt){
+            for(int i:graph[h]){
+                if(!visited[i]){
+                    visited[i]=true;
+                    cnt = bfs(i,++cnt);
+                }    
+            
+        }
+        return cnt;
+    }
+
     public static void main(String args[]) throws Exception {
-        int n = 9;
-        int[][] wires = {{1,3},{2,3},{3,4},{4,5},{4,6},{4,7},{7,8},{7,9}};
+        int n = 4;
+        int[][] wires = {{1,2},{2,3},{3,4}};
+        //int n = 9;
+        //int[][] wires = {{1,3},{2,3},{3,4},{4,5},{4,6},{4,7},{7,8},{7,9}};
 
         int answer = solution(n,wires);
         System.out.println("answer"+answer);

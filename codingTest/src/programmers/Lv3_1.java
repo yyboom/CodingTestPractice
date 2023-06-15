198fa84120243e960111ef15c6067a8427d1f574
YunYoungCho <yoonyoung37@naver.com>
2023-06-16 00:27:55 +0900

    20230615_Lv3_1_베스트 앨범

--- codingTest/src/programmers/Lv3_1.java
+++ codingTest/src/programmers/Lv3_1.java
@@ -31,21 +31,24 @@ public class Lv3_1 {
         for(int i=0;i<genres.length;i++){
             shash.put(genres[i], shash.getOrDefault(genres[i], 0)+plays[i]); // 없는 key값일 때 value를 0으로 하여 hashmap에 넣어주는 것
         }
-        int[] answer = new int[2*shash.size()];
 
         // sum값에 따라 내림차순 정렬
         // hashmap value값 기준으로 내림차순 정렬
-        List<Map.Entry<String, Integer>> entryList = new LinkedList<>(shash.entrySet()); // shash의 entry set을 list형태로 저장
-        entryList.sort(new Comparator<Map.Entry<String, Integer>>() {
-            @Override
-            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-            return o2.getValue() - o1.getValue();
-            }
-        });       
+        // List<Map.Entry<String, Integer>> entryList = new LinkedList<>(shash.entrySet()); // shash의 entry set을 list형태로 저장
+        // entryList.sort(new Comparator<Map.Entry<String, Integer>>() {
+        //     @Override
+        //     public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+        //     return o1.getValue() - o2.getValue();
+        //     }
+        // });
+        List<String> key = new ArrayList<>(shash.keySet()); // hashmap정렬은 따로 빼서 해주는게 좋음.
+        Collections.sort(key,((o1, o2) -> (shash.get(o2)).compareTo(shash.get(o1))));
 
         // 1. hash크기 만큼 돌면서 장르별로 각각의 노래 스트리밍 수 생성
         ArrayList<Music> array = new ArrayList<>();
-        for(String i : shash.keySet()){
+        for(String i : key){
+            System.out.println(i);
+            System.out.println(shash.get(i));
             ArrayList<Music> imshi = new ArrayList<>(); // 각 장르 안에 music들
             for(int j=0;j<genres.length;j++){
                 if(genres[j].equals(i)){
@@ -60,7 +63,7 @@ public class Lv3_1 {
             array.add(imshi.get(0));
             if(imshi.size()>=2) array.add(imshi.get(1));
         }
-
+        int[] answer = new int[array.size()];
         // arraylist배열로 바꿔주기(int는 어쩔 수 없이 for문을 사용해서 넣어줘야 함. primitive type이기 때문)
         // String일 때는 String arr[] = arrList.toArray(new String[arrListSize]);로 가능
         for(int i=0;i<array.size();i++){
@@ -70,7 +73,7 @@ public class Lv3_1 {
     }
     public static void main(String args[]) throws Exception {
         String[] genres = {"classic", "pop", "classic", "classic", "pop"};
-        int[] plays = {500, 600, 150, 800, 2500};
+        int[] plays = {500, 600, 150, 800, 600};
         int[] answer = solution(genres,plays);
         for(int i:answer){
             System.out.printf(i+" ");

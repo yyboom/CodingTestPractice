7d88df1802939f1c7a13c0391735cb36915ae354
YunYoungCho <yoonyoung37@naver.com>
2023-07-04 00:56:28 +0900

    20230704_Lv2_4_큰 수 만들기

--- codingTest/src/programmers/Lv2_4.java
+++ codingTest/src/programmers/Lv2_4.java
@@ -1,63 +1,31 @@
 package programmers;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
 public class Lv2_4 {
 
     // 큰 수 만들기
-    // 20230601
-    // 완전 탐색으로는 불가능함
-    // https://born2bedeveloper.tistory.com/27
-    static HashSet<Integer> hash;
+    // 20230704
+
     public static String solution(String number, int k) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder(); // 시간을 줄이기 위함
 
-        // 1. 재귀함수
-        String[] numarr = number.split("");
-        String[] outnumarr = new String[numarr.length-k];
-        boolean[] visited = new boolean[numarr.length];
+        // 1. 받아온 number값을 나눠서 배열에 넣어줌.
 
-        // 2. hash에 넣고
-        hash = new HashSet<>();
-        permutation(hash,numarr,outnumarr,visited,0,numarr.length-k);
-
-        // 3. collection으로 오름차순 정렬
-        // arraylist로 변환하여 정렬
-        ArrayList<Integer> al = new ArrayList<>(hash);
-        Collections.sort(al);
-
-        // 4. 맨 뒤의 숫자 뽑기
-        answer = Integer.toString(al.get(al.size()-1));
-
-        return answer;
-    }
-
-    // 재귀함수
-    public static void permutation(HashSet<Integer> hash, String numarr[], String outnumarr[], boolean[] visited, int depth, int r){
-        String answer="";
-        // 최종 돌아야 하는 배열의 길이만큼 다 돌았을 때
-        if(depth==r){
-            int c =0;
-            for(int i=0;i<r;i++){
-                answer += outnumarr[i];
+        int index = 0;
+        // 3. 최종 answer의 길이와 같고 거기서 제일 큰 수 를 골라서 뒤를 return 제일 max인 값을 골라서 그뒤를 전부 return
+        for(int i=0;i<number.length()-k;i++){
+            int max = 0;
+            for(int j=index;j<i+k+1;j++){
+                if(max<number.charAt(j)-'0'){
+                    max=number.charAt(j)-'0';
+                    index = j+1;
+                }
             }
-            int an = Integer.parseInt(answer); // 숫자 앞에 0 지워줌
-            System.out.println("an"+an);
-            hash.add(an);
-            return;
+            answer.append(max);
         }
 
-        // 배열의 길이만큼 도는 거
-        for(int i=0;i<numarr.length;i++){
-            if(!visited[i]){
-                visited[i] = true;
-                outnumarr[depth]=numarr[i];
-                permutation(hash,numarr, outnumarr, visited, depth+1, r);
-                visited[i]=false;
-            }
-        }
+        return answer.toString();
     }
-
     public static void main(String args[]) throws Exception {
         String numbers = "1924";
         int k = 2;
